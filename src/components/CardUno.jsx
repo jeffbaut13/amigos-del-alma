48fb1723781d6f446c37ee75f295942a2bf6ad70
jeffbaut13 <jeferssonabv@gmail.com>
2024-09-17 15:53:04 -0500

    mensaje stockout

--- src/components/CardUno.jsx
+++ src/components/CardUno.jsx
@@ -32,21 +32,33 @@ export const CardUno = ({ next }) => {
         <span className="text-xs">Por: Inter Rapidísimo</span>
       </div>
 
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-3xl leading-6">$89.000</p>
-        <p className="text-xs stock">
-          Disponibles: {stockDije !== null ? stockDije : "Cargando..."}
-        </p>
-        <hr className="border-black border w-1/3 mt-4" />
-      </div>
+      {stockDije > 1 ? (
+        <>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-3xl leading-6">$89.000</p>
+            <p className="text-xs stock">
+              Disponibles: {stockDije !== null ? stockDije : "Cargando..."}
+            </p>
+            <hr className="border-black border w-1/3 mt-4" />
+          </div>
 
-      <button onClick={next}>Iniciar</button>
+          <button onClick={next}>Iniciar</button>
 
-      <p className="NeueHassRoman text-black lg:text-xs xs:text-base px-8 text-center lg:w-[65%] xs:w-full">
-        Este collar de finas terminaciones enmarcado en un contenedor de cuero y
-        un estético dije que llevará impresa su foto, será el símbolo ideal para
-        que todo el mundo reconozca a tu Amigo del Alma.
-      </p>
+          <p className="NeueHassRoman text-black lg:text-xs xs:text-base px-8 text-center lg:w-[65%] xs:w-full">
+            Este collar de finas terminaciones enmarcado en un contenedor de
+            cuero y un estético dije que llevará impresa su foto, será el
+            símbolo ideal para que todo el mundo reconozca a tu Amigo del Alma.
+          </p>
+        </>
+      ) : (
+        <>
+          <p className="NeueHassRoman text-black lg:text-base xs:text-base px-8 text-center lg:w-[65%] xs:w-full">
+            Las existencias de nuestro collar Amigos del Alma se han agotado.
+            Gracias por querer sumarte a este homenaje a esos amigos que lo
+            entregan todo.
+          </p>
+        </>
+      )}
     </div>
   );
 };
